f16dd68bc99239ed30072b2b1e369cc46e451e50
cary fitzhugh <cary.fitzhugh.ctr@navy.mil>
2026-08-19 10:33:58 -0400

    updated

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p ~/.vim/{swap,backup,undo}
 
 rm ~/.vimrc
-ln -s ~/.vimrc $SCRIPT_DIR/vim/VIMRC
+ln -s $SCRIPT_DIR/vim/VIMRC ~/.vimrc
 
 PACKAGES=(
     vim
