4ba9c8dbfcd387c88ea181ff4908f7b808d00ea5
cary fitzhugh <cary.fitzhugh.ctr@navy.mil>
2026-08-19 10:40:02 -0400

    fixing vimrc

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,8 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p ~/.vim/{swap,backup,undo}
 
-rm ~/.vimrc
-ln -s $SCRIPT_DIR/vim/VIMRC ~/.vimrc
+echo "source $SCRIPT_DIR/vim/VIMRC" > ~/.vimrc
 
 PACKAGES=(
     vim
